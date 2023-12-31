c0ad67698c060ac4a0f834ea65e5b94fdd7f3c3b
A_pomp <amitpom14@gmail.com>
2023-12-31 13:04:01 +0200

    3112 server and design commit

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+// Import the express module
+const express = require('express');
+
+// Create an instance of express
+const app = express();
+
+// Serve static files from the same directory as this script
+app.use(express.static('.'));
+
+// When a GET request is made to the root, send index.html
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  // TODO: Validate and authenticate user
+  // If valid, redirect to user's dashboard
+  // If invalid, send an error message
+});
+
+app.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+  // TODO: Validate input, hash password, and store in database
+  // Redirect to login page or directly log them in
+});
+
+// Start the server on port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
